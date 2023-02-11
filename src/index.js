75ee25e59925dde26db5b0854699e92763117067
Hakan D <68669489+hakandrsn@users.noreply.github.com>
2023-02-11 18:24:12 +0300

    index last state

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
+import {CssBaseline} from "@mui/material";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
+import ProductLayout from "./layout/ProductLayout";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+        <Provider store={store}>
+            <CssBaseline/>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" render={(props)=> <ProductLayout {...props} />} />
+                    <Redirect to="/products" from="/" />
+                </Switch>
+            </BrowserRouter>
+        </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
